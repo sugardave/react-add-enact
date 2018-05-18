ad1225c050736c69be67308c724bb1cd637f3b7e
Dave Freeman <dave.freeman@lge.com>
2018-05-18 16:51:40 -0500

    fixup

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -258,12 +258,6 @@ module.exports = {
     // ensure the locale data files and the resource files are copied during
     // the build to the output directory.
     new ILibPlugin(),
-    // Moment.js is an extremely popular library that bundles large locale files
-    // by default due to how Webpack interprets its code. This is a practical
-    // solution that requires the user to opt into importing specific locales.
-    // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
-    // You can remove this if you don't use Moment.js:
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ZirconDecorator from '@enact/zircon/ZirconDecorator';
 
 import MainView from './MainView';
 
@@ -10,4 +11,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default ZirconDecorator(App);
